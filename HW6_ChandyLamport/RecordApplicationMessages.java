6d979437973475090d0a3f8feed6282eac528aef
Deepthi-Vis-011424855 <vnath.deepthi@gmail.com>
2017-10-16 19:14:12 -0700

    Code refactoring

--- HW6_ChandyLamport/RecordApplicationMessages.java
+++ HW6_ChandyLamport/RecordApplicationMessages.java
@@ -6,100 +6,38 @@ import java.util.List;
 import java.util.Map;
 
 public class RecordApplicationMessages extends Thread {
-  Buffer channel;
-  Processor p;
-  Map <Buffer, Integer> lastIndexMap = null;
-  
-  public RecordApplicationMessages(Buffer channel, Processor p) {
-    this.channel = channel;
-    this.p=p;
-    lastIndexMap = new HashMap <Buffer, Integer> ();
-  }
-  
-  
-  @Override
-  public void run() {
-    int lastIdx = channel.getTotalMessageCount() - 1;
-    List<Message> recordedMessagesSinceMarker = new ArrayList<>();
-    while(lastIdx >= 0) {
-      Message message = channel.getMessage(lastIdx);
-      if(MessageType.MARKER.equals(message.getMessageType()))
-        break;
-      recordedMessagesSinceMarker.add(message);
-      lastIdx--;
-    }
-    p.channelState.put(channel, recordedMessagesSinceMarker);
-  }
-    
-    
-    
- 
-  /*
-  public void run() {
-    
-    int lastIdx=-1;
-    
-    System.out.println();
-    if(!lastIndexMap.containsKey(channel)) {
-      lastIdx = channel.getTotalMessageCount();
-      lastIndexMap.put(channel, lastIdx);
-     } else
-       lastIdx = lastIndexMap.get(channel);
-    recordChannel(channel, lastIdx);  
-  }
-  
-  
-    public void recordChannel(Buffer channel, int lastIdx) {
-    
-    List<Message> recordedMessagesSinceMarker = new ArrayList<>();
-    
-    try {
-      synchronized(this) {
-      while (p.channelMarkerCount.get(p)==1) {
-        
-        System.out.println("@@@@@@ I started recording on channel " + channel.getLabel() + " after index: " + lastIdx);
-        System.out.println(" Channel " + channel.getLabel() + " Message count is: " + channel.getTotalMessageCount());
-        if(channel.getMessage(lastIdx)!=null)
-          recordedMessagesSinceMarker.add(channel.getMessage(lastIdx));
-       
-      }
-      System.out.println();
-      System.out.println("The channelmarkercount is no more just 1. Its more!");
-      p.channelState.put(channel, recordedMessagesSinceMarker);
-//      System.out.println("INTERRUPTED: Channel msg count before was: " + lastIdx + ". Channel msg count now is: " + channel.getTotalMessageCount());
-//      System.out.println("Interrupted channel " + channel.getLabel() + ". Put the channel and its message in the global state.");
-      System.out.println();
-      }
-    } catch (Exception e) {
-      
-      throw new RuntimeException("Recording of channel " + channel.getLabel() + " interrupted...");
-    } 
-    
-    
-    
-//    try {
-//      while (p.channelMarkerCount.get(p)==1) {
-//        i= count;
-//        while(i<=count) {
-//          System.out.println("~~~~~~~~~~~~~~~~~~~ For channel" + channel.getLabel()+ "i is: " + i);
-//          System.out.println("___________________ count for channel" + channel.getLabel()+ " is: " + count);
-//          recordedMessagesSinceMarker.add(channel.getMessage(i));
-//          i++;
-//          count = channel.getTotalMessageCount();
-//          Thread.sleep(3000);
-//        }
-//       
-//      }
-//      p.channelState.put(channel, recordedMessagesSinceMarker);
-//      
-//    } catch (InterruptedException e) {
-//      throw new RuntimeException("Recording of channel " + channel + " interrupted...");
-//    }
-    
-    
-    
- 
+	Buffer channel;
+	Processor p;
+	List<Message> recordedMessagesSinceMarker;
 
-  }
-*/
+	public RecordApplicationMessages(Buffer channel, Processor p) {
+		this.channel = channel;
+		this.p = p;
+		recordedMessagesSinceMarker = new ArrayList<>();
+	}
+
+	@Override
+	public void run() {
+		int lastIdx = channel.getTotalMessageCount() - 1;
+		
+		while (true) {
+			if (lastIdx > 0) {
+				int count = channel.getTotalMessageCount() - 1;
+				Message message = channel.getMessage(count);
+
+				if (MessageType.MARKER.equals(message.getMessageType())) {
+					System.out.println("*********Duplicate marker received on channel "+channel.getLabel()+"************");
+					System.out.println("Interrupting thread "+Thread.currentThread().getName());
+					Thread.currentThread().interrupt();
+					break;
+				} 
+			} else {
+				try {
+					Thread.currentThread().sleep(1000);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 }
