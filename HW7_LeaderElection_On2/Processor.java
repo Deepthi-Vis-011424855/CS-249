fced88889659638c14bdfceabf46590e24a262f4
Deepthi-Vis-011424855 <vnath.deepthi@gmail.com>
2017-10-21 22:32:33 -0700

    SLight modifications in output and Processor.java

--- HW7_LeaderElection_On2/Processor.java
+++ HW7_LeaderElection_On2/Processor.java
@@ -28,24 +28,29 @@ public class Processor {
 		int otherProcessorId;
 		if (!this.queue.isEmpty()) {
 			otherProcessorId = (int) this.queue.poll();
-			System.out.println("Processor " + this.id + " received message from " + otherProcessorId);
-
+			if (!this.isLeader) {
+				System.out.println("Processor " + this.id + " received message from " + otherProcessorId);
+			}
 			if (otherProcessorId == -1 && !(this.isLeader)) {
 				System.out.println();
 				System.out.println("Processor P" + this.id + " TERMINATED as a non-leader.");
 				send(-1);
 			} else if (otherProcessorId < this.id) {
-				System.out.println();
-				System.out.println("Processor " + this.id + " swallowed the Processor " + otherProcessorId);
+				if (!this.isLeader) {
+					System.out.println();
+					System.out.println("Processor " + this.id + " swallowed the message <<" + otherProcessorId
+							+ ">> from Processor" + otherProcessorId);
+				}
 			} else if (otherProcessorId == this.id) {
 				System.out.println();
 				System.out.println("RECEIVED MY OWN ID AGAIN");
-				System.out.println("The leader is : Processor " + this.id);
+				System.out.println("***Sending <<-1>> as termination message***");
 				this.isLeader = true;
 				send(-1);
 			} else {
 				System.out.println();
-				System.out.println("Processor " + this.id + " forwarding message <<" +otherProcessorId+ ">> from " + otherProcessorId);
+				System.out.println("Processor " + this.id + " forwarding message <<" + otherProcessorId + ">> from "
+						+ otherProcessorId);
 				send(otherProcessorId);
 			}
 		}
